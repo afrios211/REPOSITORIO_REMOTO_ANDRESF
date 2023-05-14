c99bf59e4be2a1f4bb5d9ae12a82c3f985b30bcc
ANDRES F <afrios211@gmail.com>
2023-05-13 21:59:17 -0500

    actualizacion

---
+++ PromedioDe3Notas/src/main/java/cuenta/account.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cuenta;
+
+/**
+ *
+ * @author ANDRES F
+ */
+public class account {
+   private String numeroCuenta = "";
+   private float saldo;
+    
+    public account(){
+        
+     
+       
+    }
+
+    /**
+     * @return the numeroCuenta
+     */
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    /**
+     * @param numeroCuenta the numeroCuenta to set
+     */
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public float getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+    
+}
